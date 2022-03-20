dea66f9d3c418ebe6ec1f59d1ca6f6528ad8f9b6
mrdeangray <mrdeangray@gmail.com>
2022-03-20 18:48:10 -0400

    add update route and function

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ res.render("products/new")
     // res.send(`new product`)
 })
 
+app.post("/products", async (req, res)=>{
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
+})
 
 app.get("/products", async (req, res)=>{
     const products = await Product.find({})
@@ -48,3 +53,18 @@ const product = await Product.findById(id)
 res.render("products/show", {product})
 // res.send(`the selected item is: ${product.name}`)
 })
+
+app.put("/products/:id", async(req, res)=>{
+    const {id} = req.params
+ const product = await Product.findByIdAndUpdate(id, req.body, {runValidators:true, new:true})
+res.redirect("/products")
+ // res.send(`the selected item is: ${product.name}`)
+ })
+ 
+
+app.get("/products/:id/edit", async(req, res)=>{
+    const {id} = req.params
+ const product = await Product.findByIdAndUpdate(id)
+ res.render("products/edit", {product})
+ // res.send(`the selected item is: ${product.name}`)
+ })
